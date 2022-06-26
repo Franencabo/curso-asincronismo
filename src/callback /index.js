1f88d6efdde08f0f36ec7e032e77ae5566045cd2
Fran <franencabo@gmail.com>
2022-06-26 17:35:20 +0200

    Callback y FetchData

---
+++ src/callback /index.js
@@ -0,0 +1,35 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function rest(num1, num2) {
+    return num1 - num2;
+}
+
+function mult(num1, num2) {
+    return num1 * num2;
+}
+
+function div(num1, num2) {
+    return num1 / num2;
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1, num2);
+};
+
+console.log(calc(2, 2, sum));
+
+
+
+// SetTimeout ya es un callback de por sí
+setTimeout(function () {
+    console.log('Hola JavaScript');
+}, 2000)
+
+
+function gretting(name) {
+    console.log(`Hola ${name}`);
+}
+
+setTimeout(gretting, 2000, 'Fran');
